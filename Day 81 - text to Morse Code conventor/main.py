8fbf9d7ca9c8f79fb4c6560709faf1b5b74e4875
Bothaina Karakrah <63556283+Bothaina-Karakrah@users.noreply.github.com>
2022-12-20 17:08:34 +0200

    Create main.py

---
+++ Day 81 - text to Morse Code conventor/main.py
@@ -0,0 +1,51 @@
+morse_code_rules = {
+    'a': '·−',
+    'b': '−···',
+    'c': '−·−·',
+    'd': '−··',
+    'e': '·',
+    'f': '··−·',
+    'g': '−−·',
+    'h': '····',
+    'i': '··',
+    'j': '·−−−',
+    'k': '−·−',
+    'l': '·−··',
+    'm': '−−',
+    'n': '−·',
+    'o': '−−−',
+    'p': '·−−·',
+    'q': '−−·−',
+    'r': '·−·',
+    's': '···',
+    't': '−',
+    'u': '··−',
+    'v': '···−',
+    'w': '·−−',
+    'x': '−··−',
+    'y': '−·−−',
+    'z': '−−··',
+    '0': '−−−−−',
+    '1': '·−−−−',
+    '2': '··−−−',
+    '3': '···−−',
+    '4': '····−',
+    '5': '·····',
+    '6': '−····',
+    '7': '−−···',
+    '8': '−−−··',
+    '9': '−−−−·',
+    ' ': '/'
+}
+
+print("Hello There! Wellcome to Morse Code Conventor")
+print("Type * if you want to stop.")
+
+while True:
+  text_to_convert = input("Type the text you want to convert:\n")
+  if text_to_convert == '*':
+    break
+  converted_text = ""
+  for c in text_to_convert:
+    converted_text+=morse_code_rules[c]
+  print(converted_text)
